66de3fe207d1d41268d4a310c733d1969aa0f02d
debbie-tommy <68273014+debbie-tommy@users.noreply.github.com>
2025-02-09 23:07:00 +0530

    Discriminator class

--- FakeImages.py
+++ FakeImages.py
@@ -19,3 +19,17 @@ class Generator(nn.Module):
 
     def forward(self, x):
         return self.model(x)
+
+# Discriminator
+class Discriminator(nn.Module):
+    def __init__(self, input_dim):
+        super(Discriminator, self).__init__()
+        self.model = nn.Sequential(
+            nn.Linear(input_dim, 128),
+            nn.LeakyReLU(0.2),
+            nn.Linear(128, 1),
+            nn.Sigmoid()  # Output probability
+        )
+
+    def forward(self, x):
+        return self.model(x)
