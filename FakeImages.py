707ffe73af299fe0ae1cf91fd2c804c8c8594156
debbie-tommy <68273014+debbie-tommy@users.noreply.github.com>
2025-02-09 23:05:27 +0530

    update imports

---
+++ FakeImages.py
@@ -0,0 +1,8 @@
+import torch
+import torch.nn as nn
+import torch.optim as optim
+import torchvision.datasets as dsets
+import torchvision.transforms as transforms
+import matplotlib.pyplot as plt
+import numpy as np
+
