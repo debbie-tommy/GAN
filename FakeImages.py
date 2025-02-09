d42edfe9fd489033323fbe44f3f991fab6c4ec47
debbie-tommy <68273014+debbie-tommy@users.noreply.github.com>
2025-02-09 23:06:21 +0530

    generator class

--- FakeImages.py
+++ FakeImages.py
@@ -6,3 +6,16 @@ import torchvision.transforms as transforms
 import matplotlib.pyplot as plt
 import numpy as np
 
+# Generator
+class Generator(nn.Module):
+    def __init__(self, input_dim, output_dim):
+        super(Generator, self).__init__()
+        self.model = nn.Sequential(
+            nn.Linear(input_dim, 128),
+            nn.ReLU(),
+            nn.Linear(128, output_dim),
+            nn.Tanh()  # Output values between -1 and 1
+        )
+
+    def forward(self, x):
+        return self.model(x)
